9836f7a79f335d786b709dfbd4380a9aac9a73d2
liuziting <57311725+liu-ziting@users.noreply.github.com>
2024-01-03 13:55:10 +0800

    更新 _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,17 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     {/* 在这里添加Google Analytics代码 */}
-                    <script type="text/javascript">
-                        (function(c,l,a,r,i,t,y){
-                            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                        })(window, document, "clarity", "script", "kfx5eatp07");
-                    </script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "kfx5eatp07");
+              `
+                        }}
+                    />
                 </Head>
                 <body>
                     <Main />
